48db9d469c72a974ca6f3e08542efb1970b068d4
Edu <eduardo.dacruz@dcomp.sor.ufscar.br>
2018-10-27 18:30:59 -0300

    Home screen 4

---
+++ app/src/main/java/org/drulabs/localdash/BattleActivity.java
@@ -0,0 +1,14 @@
+package org.drulabs.localdash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+
+public class BattleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_battle);
+    }
+}
